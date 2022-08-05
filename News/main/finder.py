d9c4856e80f71de4e4af5aea4f0af4dba3eef629
abrham <getnetabrham@gmail.com>
2022-08-05 15:53:55 +0300

    date modification

--- News/main/finder.py
+++ News/main/finder.py
@@ -1,9 +1,10 @@
 from pygooglenews import GoogleNews
 from newscatcher import Newscatcher
 import requests
+from datetime import date
 
 KEY = "d7e59f811066412d846f00c8217aa6c6"
-
+today = date.today().strftime("%Y-%m-%d")
 # top news
 def ethio_news():
     google_news = GoogleNews(country='et')
@@ -12,18 +13,18 @@ def ethio_news():
 
 # world news
 def world_news():
-    url = f"https://newsapi.org/v2/everything?q=world&from=2022-05-18&sortBy=popularity&apiKey={KEY}"
+    url = f"https://newsapi.org/v2/everything?q=world&from={today}&sortBy=popularity&apiKey={KEY}"
     news = requests.get(url).json()
     return [{'title':news['title'],'link':news['url'], 'image': news['urlToImage']} for news in news['articles']]
 
 # africa news
 def africa_news():
-    url = f"https://newsapi.org/v2/everything?q=Africa&from=2022-05-18&sortBy=popularity&apiKey={KEY}"
+    url = f"https://newsapi.org/v2/everything?q=Africa&from={today}&sortBy=popularity&apiKey={KEY}"
     news = requests.get(url).json()
     return [{'title':news['title'],'link':news['url'], 'image': news['urlToImage']} for news in news['articles']]
 
 # europe news
 def europe_news():
-    url = f"https://newsapi.org/v2/everything?q=europe&from=2022-05-18&sortBy=popularity&apiKey={KEY}"
+    url = f"https://newsapi.org/v2/everything?q=europe&from={today}&sortBy=popularity&apiKey={KEY}"
     news = requests.get(url).json()
     return [{'title':news['title'],'link':news['url'], 'image': news['urlToImage']} for news in news['articles']]
